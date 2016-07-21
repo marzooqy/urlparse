46ea34323fb1c58a3ba998de413db54609e022af
marzooqy <marzooqy@users.noreply.github.com>
2016-07-21 22:43:55 +0300

    Add files via upload

---
+++ urlparse.py
@@ -0,0 +1,13 @@
+import urllib.parse
+
+def parse(urlstring):
+	parts = urllib.parse.urlparse(urlstring)
+	query = urllib.parse.parse_qs(parts[4])
+	
+	for name in query:
+		query[name] = query[name][0]
+		
+	return {'scheme': parts[0], 'netloc': parts[1], 'path': parts[2], 'params': parts[3], 'query': query, 'fragment': parts[5]}
+
+def unparse(parts):
+	return urllib.parse.urlunparse((parts['scheme'], parts['netloc'], parts['path'], parts['params'], urllib.parse.urlencode(parts['query']), parts['fragment']))
